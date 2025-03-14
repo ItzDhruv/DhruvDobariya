2c727d34b99de2575619b77d50517a6987b196f2
ItzDhruv <dhruvdobariya897@gmail.com>
2025-03-15 00:31:47 +0530

    finle commit

--- Portfolio/src/components/About/AboutCard.js
+++ Portfolio/src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple"> DHRUV DOBARIYA</span> { " "}
-            from <span className="purple"> Gujarat, India.</span>
+            from <span className="purple"> GUJARAT, INDIA.</span>
             <br />
             I am currently employed as a software developer at CodeMinto.
             <br />
