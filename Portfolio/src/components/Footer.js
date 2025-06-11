63ab07031829648bab4dc9dde3ae53fc31ba93af
ItzDhruv <dhruvdobariya897@gmail.com>
2025-06-11 14:55:10 +0530

    in link

--- Portfolio/src/components/Footer.js
+++ Portfolio/src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/dhruv_dobariay_20"
+                href="https://www.instagram.com/dhruv_dobariya_20"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
